51f853ace53fa0982e7a7a3fb725f8e29c9ca8fd
James Henning <jamesniklas@hotmail.de>
2023-06-27 14:52:44 +0200

    Try

--- app/OddspediaWidget.js
+++ app/OddspediaWidget.js
@@ -9,7 +9,7 @@ export default function OddspediaWidget() {
       window.oddspediaWidgetOddsComparisonPopularSportsLeagues = {
         api_token: "1802fa7b754c1a082862aa19948c30499bcd952be46462a15b5874b481e3",
         type: "odds-comparison",
-        domain: "widget-trial.vercel.app/",
+        domain: "widget-component-trial.vercel.app/",
         selector: "oddspedia-widget-container",
         width: "1200",
         theme: "1",
